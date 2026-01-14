397308afffce26e537dd030037cacef034987fd6
adiY666 <adi.yekutieljr@gmail.com>
2026-01-14 20:15:21 +0200

    cleared Robot.java, but wtf happened in here?!?

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,18 +4,10 @@
 
 package frc.robot;
 
-import com.revrobotics.spark.SparkBase;
-import com.revrobotics.spark.SparkLowLevel;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkBaseConfig;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
 
-    SparkMax spark1 = new SparkMax(1, SparkLowLevel.MotorType.kBrushless);
-    SparkMax spark2 = new SparkMax(4, SparkLowLevel.MotorType.kBrushless);
     @Override
     public void robotInit() {
 
@@ -29,10 +21,6 @@ public class Robot extends TimedRobot {
     @Override
     public void disabledInit() {
         CommandScheduler.getInstance().cancelAll();
-        spark1.stopMotor();
-        spark2.stopMotor();
-        spark1.configure(new SparkMaxConfig().idleMode(SparkBaseConfig.IdleMode.kBrake), SparkBase.ResetMode.kNoResetSafeParameters, SparkBase.PersistMode.kNoPersistParameters);
-        spark2.configure(new SparkMaxConfig().idleMode(SparkBaseConfig.IdleMode.kBrake), SparkBase.ResetMode.kNoResetSafeParameters, SparkBase.PersistMode.kNoPersistParameters);
     }
 
     @Override
@@ -42,8 +30,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void autonomousInit() {
-        spark1.set(-0.2);
-        spark2.set(-0.2);
+
     }
 
     @Override
@@ -53,8 +40,6 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopInit() {
-        spark1.set(0.2);
-        spark2.set(0.2);
     }
 
     @Override
